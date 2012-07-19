553d18dc77ae53ab8e520d2bd8f9fff2f7bfad87
aristotle9 <lanfan.1987@gmail.com>
2012-07-19 18:12:50 +0800

    haskell amf3 & sol parse tool. Copyright (C) aristotle9(lanfan.1987@gmail.com)

---
+++ amf3.hs
@@ -0,0 +1,210 @@
+import Control.Monad.State
+import Data.Binary.Strict.Get
+import qualified Data.ByteString as B
+import Data.ByteString.UTF8 (toString)
+import Control.Applicative ((<$>))
+import Data.Bits ((.|.), (.&.), shiftL, shiftR)
+import Text.JSON.Types
+
+import Text.JSON (encode)
+import System.Environment (getArgs)
+import System.IO
+import Data.ByteString.Char8 (pack)
+
+data AMFObjectTrait = AMFObjectTrait {
+    isExternalizable :: Bool,
+    isDynamic :: Bool,
+    getClassName :: String,
+    getMemberNames :: [String]
+} deriving (Show, Eq)
+
+data AMFCache = AMFCache {
+    getObjectCache :: [JSValue],
+    getTraitCache :: [AMFObjectTrait],
+    getStringCache :: [JSValue]
+} deriving (Show, Eq)
+
+type AMF a = StateT AMFCache Get a
+
+cacheObject :: JSValue -> AMF ()
+cacheObject obj = do
+    cache@(AMFCache {getObjectCache = xs}) <- get
+    put cache {getObjectCache=obj:xs}
+
+refObject :: Int -> AMF JSValue
+refObject n = do
+    AMFCache {getObjectCache = xs} <- get
+    let len = length xs
+    if len <= n then
+        fail $ "There is no object reffered by " ++ (show n)
+    else
+        return $ xs !! (len - n - 1)
+
+cacheTrait :: AMFObjectTrait -> AMF ()
+cacheTrait trait = do
+    cache@(AMFCache {getTraitCache = xs}) <- get
+    put cache {getTraitCache = trait : xs}
+
+refTrait :: Int -> AMF AMFObjectTrait
+refTrait n = do
+    AMFCache {getTraitCache = xs} <- get
+    let len = length xs
+    if len <= n then
+        fail $ "There is no trait reffered by " ++ (show n)
+    else
+        return $ xs !! (len - n - 1)
+
+cacheString :: JSValue -> AMF ()
+cacheString s = do
+    cache@(AMFCache {getStringCache = xs}) <- get
+    put cache {getStringCache = s : xs}
+
+refString :: Int -> AMF JSValue
+refString n = do
+    cache@(AMFCache {getStringCache = xs}) <- get
+    let len = length xs
+    if len <= n then
+        fail $ "There is no string reffered by " ++ (show n)
+    else 
+        return $ xs !! (len - n - 1)
+
+readPadding :: AMF ()
+readPadding = do
+    x <- fromIntegral <$> lift getWord8
+    if x == 0 then return () else do
+        lift $ fail "There must be a null char for separating."
+
+-- a usefull function
+readUInt29 :: AMF Integer
+readUInt29 = do
+    let readUByte = toInteger <$> lift getWord8
+    a0 <- readUByte
+    if a0 < 128 then return $! a0 else do
+        let a1 = (a0 .&. 127) `shiftL` 7
+        a2 <- readUByte
+        if a2 < 128 then return $! a1 .|. a2 else do
+            let a3 = (a1 .|. a2 .&. 127) `shiftL` 7
+            a4 <- readUByte
+            if a4 < 128 then return $! a3 .|. a4 else do
+                let a5 = (a3 .|. a4 .&. 127) `shiftL` 8
+                a6 <- readUByte
+                return $! a5 .|. a6
+
+--read int
+readInt :: AMF JSValue
+readInt = do
+    i <- readUInt29
+    return $ JSRational False $ toRational $! (i `shiftL` 3) `shiftR` 3
+
+--read double
+readDouble :: AMF JSValue
+readDouble = do
+    num <- lift getFloat64host
+    return $ JSRational False $ toRational num
+
+--read a string
+readString :: AMF JSValue
+readString = do
+    l <- readUInt29
+    let ref = l .&. 1
+    if ref == 0
+    then
+        refString $ fromIntegral $! l `shiftR` 1
+    else do
+        let str_len = fromIntegral $! l `shiftR` 1
+        b_str <- lift $ getByteString str_len
+        let str = (JSString . JSONString . toString) b_str
+        cacheString str
+        return str
+
+--read an array
+readArray :: AMF JSValue
+readArray = do
+    l <- readUInt29
+    let ref = l .&. 1
+    if ref == 0 then
+        refObject $ fromIntegral $! l `shiftR` 1
+    else do
+        let readAttr xs = do
+            (JSString (JSONString str)) <- readString
+            if str == "" then
+                return $ reverse xs
+            else do
+                dt <- readData
+                readAttr ((str,dt):xs)
+        pairs <- readAttr []
+        let arr_len = l `shiftR` 1
+        let readArr xs n len = do
+            if n == len then
+                return $ reverse xs
+            else do
+                dt <- readData
+                readArr ((show n, dt):xs) (n + 1) len
+        arrs <- readArr [] 0 arr_len
+        let ret =   if length pairs == 0 then
+                        JSArray [dt | (_,dt) <- arrs]
+                    else
+                        JSObject $ JSONObject $ pairs ++ arrs
+        cacheObject ret
+        return ret
+
+--read data
+readData :: AMF JSValue
+readData = do
+    flag <- fromIntegral <$> lift getWord8
+    case flag of
+        0 -> return JSNull
+        1 -> return JSNull
+        2 -> return $ JSBool False
+        3 -> return $ JSBool True
+        4 -> readInt
+        5 -> readDouble
+        6 -> readString
+        9 -> readArray
+        _ -> return JSNull
+
+--read sol file header
+readSOLHeader :: AMF (Int, String, Int)
+readSOLHeader = do
+    version <- fromIntegral <$> lift getWord16be
+    l <- lift getWord32be
+    signature <- lift $ getByteString 10
+    l <- lift getWord16be
+    name <- toString <$> (lift $ getByteString $ fromIntegral l)
+    amfversion <- fromIntegral <$> lift getWord32be
+    return (version, name, amfversion)
+
+--read sol file
+readSOL :: AMF JSValue
+readSOL = do
+    (_,_,3) <- readSOLHeader -- amf3
+
+    let readPairs = do
+        eof <- lift isEmpty
+        if eof then
+            return []
+        else do
+            (JSString (JSONString name)) <- readString
+            value <- readData
+            readPadding
+            ((name, value):) <$> readPairs
+
+    --ret <- reverse <$> readPairs
+    ret <- readPairs
+    return $ JSObject $ JSONObject ret
+
+--decode sol file
+parseSOL :: B.ByteString -> Either String JSValue
+parseSOL src = jsvalue where
+    (jsvalue, _) = runGet getter src
+    getter = evalStateT readSOL state
+    state = AMFCache [] [] []
+
+main = do
+    file:_ <- getArgs
+    handle <- openFile file ReadMode
+    contents <- B.hGetContents handle
+    let ret = parseSOL contents
+    case ret of
+        Right jsvalue -> putStrLn $ encode jsvalue
+        Left message -> putStrLn $ "ParseError: " ++ message
